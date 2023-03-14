8e2297e5e2dfd2731f3ba9d45f37ac52762bcde4
Keks <keks@htmlacademy.ru>
2023-03-14 22:18:10 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/utils.js
@@ -0,0 +1,74 @@
+const getIdCreator = (startNumber = 1) => {
+  let id = startNumber;
+
+  return () => id++;
+};
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomArrayElement = (elements) => elements[Math.floor(Math.random() * elements.length)];
+
+const isPalindrome = (str) => {
+  const reverseStr = (str.split('').reverse().join(''));
+
+  return str === reverseStr;
+};
+
+const formingStringChar = (str, num, addStr) => {
+  const start = str.length;
+  const finish = addStr.length;
+  const count = num - start;
+  let newStr = '';
+  let secStr = '';
+
+  if (str.length >= num) {
+    return str;
+  }
+
+  if (finish === 1) {
+    return addStr.repeat(count) + str;
+  }
+
+  if (finish > count) {
+    for (let i = 0; i < count; i++) {
+      newStr += addStr[i];
+    }
+
+    return newStr + str;
+  }
+
+  for (const char of addStr) {
+    newStr += char;
+  }
+  for (let i = 0; i < count - finish; i++) {
+    secStr += addStr[i];
+  }
+
+  return secStr + newStr + str;
+};
+
+const checkStringLength = (str, maxLength) => str.length <= maxLength;
+
+const extractNumbers = (str) => {
+  let numberStr = '';
+
+  for (let i = 0; i <= str.length; i++) {
+    const letter = parseInt(str[i], 10);
+    if (!isNaN(letter)) {
+      numberStr += letter;
+    }
+  }
+
+  if (numberStr.length) {
+    return parseInt(numberStr, 10);
+  }
+
+  return NaN;
+};
+
+checkStringLength('function', 5);
+extractNumbers('local2 56gg');
+isPalindrome ('топот');
+formingStringChar ('g', 3, 'qwerty');
+
+export { getIdCreator, getRandomNumber, getRandomArrayElement };
